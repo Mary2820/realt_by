038f252977a3c3adb8ef64f0eb478f96d1fe0011
Mary2820 <mdedul2820@gmail.com>
2023-11-07 16:55:29 +0100

    Added: Empty test.

---
+++ src/test/java/tests/searchapartments/test.java
@@ -0,0 +1,11 @@
+package tests.searchapartments;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class test {
+    @Test
+    public void checkIsSelectedRegion(){
+
+    }
+}
